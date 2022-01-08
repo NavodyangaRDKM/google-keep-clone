8b9beaef3f4c4e47f6ed271b3aa2b6e0a114ab31
NavodyangaRDKM <navodyangardkm@gmail.com>
2022-01-08 18:38:17 +0530

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function CreateArea(props) {
+    return (
+        <div>
+            <form>
+                <input type='text' placeholder='Take a note...........'>
+
+                </input>
+            </form>
+        </div>
+    );
+}
+
+export default CreateArea;
